580977c7c073b31c015e92ebd2e445053624406d
evkortright <ekortright@louisianaindex.com>
2020-10-22 01:09:24 -0500

    Changed display of  each card

--- react/List.js
+++ react/List.js
@@ -12,8 +12,8 @@ const List = (props) => {
 <div class="card" style={{width: "18rem"}} key={cliente._id}>
   <div class="card-body">
     <h5 class="card-title">{cliente._source.nombre}</h5>
-    <p class="card-text">{cliente._source.score}</p>
-    <a href="#" class="btn btn-primary">Evalua</a>
+    <p class="card-text">{cliente._source.notas}</p>
+    <a href="#" class="btn btn-primary">{cliente._source.score}</a>
   </div>
 </div>
           );
